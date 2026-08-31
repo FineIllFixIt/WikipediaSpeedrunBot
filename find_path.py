fe6c5c2e79a583464cd684e6dbc1c9c196adedff
FineIllFixIt <fineillfixityt@gmail.com>
2026-08-30 23:20:06 -0600

    adding another website
    Adding bot implementation for wikispeedrun.org and fixing some bugs.

--- find_path.py
+++ find_path.py
@@ -86,14 +86,16 @@ def get_title_option(title):
         
     return results
 
-def run_wiki_bot(path, website="wikipedia.org"):
+def run_wiki_bot(path, website="wikipedia.org", wiki_speedruns_lobby_info={}):
     start_url = ""
     search_url = ""
+    sleep_time = 0.1
     if website == "wikipedia.org":
         start_url = "https://en." + website + "/wiki/" + path[0]
         search_url = "https://en." + website + "/wiki/"
         driver.get(start_url)
-    elif website == "wikispeedruns.com":
+    elif website == "wikispeedruns.com" and wiki_speedruns_lobby_info == {}:
+        sleep_time = 0.1
         # Set up wikipedia speedruns for correct pages (XPATH is pretty rigid so it might need to be updated often
         # but the inputs and start button don't have an id so not really a better way to select them.)
         start_url = "https://" + website
@@ -114,18 +116,64 @@ def run_wiki_bot(path, website="wikipedia.org"):
         time.sleep(1)
         skip_button = driver.find_element(By.ID, "start-btn")
         skip_button.click()
+    elif website == "wikispeedruns.com" and "lobby_number" in wiki_speedruns_lobby_info:
+        sleept_time = 0.1
+        start_url = "https://" + website + "/lobby/" + wiki_speedruns_lobby_info['lobby_number']
+        search_url = "/wiki/"
+        driver.get(start_url)
+        name_input = driver.find_element(By.ID, "name")
+        name_input.clear()
+        name_input.send_keys(wiki_speedruns_lobby_info['username'])
+        pass_input = driver.find_element(By.ID, "desc")
+        pass_input.clear()
+        pass_input.send_keys(wiki_speedruns_lobby_info['lobby_password'])
+        submit_button = driver.find_element(By.XPATH, '//*[@id="joinForm"]/button')
+        submit_button.click()
+        time.sleep(1)
+        start_link = None
+        try:
+            print(f"{start_url}/play/{wiki_speedruns_lobby_info['prompt_number']}")
+            start_link = driver.find_element(By.CSS_SELECTOR, f'a[href="/lobby/{wiki_speedruns_lobby_info['lobby_number']}/play/{wiki_speedruns_lobby_info['prompt_number']}"]')
+        except:
+            print("Prompts table wasn't found. Code is outdated or login information was incorrect.")
+            return None, "Something went wrong loading your data"
+        input("All set! Press enter to start.")
+        start_link.click()
+        time.sleep(1)
+        skip_button = driver.find_element(By.ID, "start-btn")
+        skip_button.click()
+    elif website == "wikispeedrun.org":
+        start_url = "https://" + website + "/settings"
+        search_url = "/wiki/"
+        driver.get(start_url)
+        time.sleep(1)
+        start_input = driver.find_element(By.ID, 'startArticle')
+        start_input.clear()
+        start_input.send_keys(path[0].replace("-", " ").replace("_", " ").title())
+        time.sleep(2)
+        start_input.send_keys(Keys.RETURN)
+        end_input = driver.find_element(By.ID, 'endArticle')
+        end_input.clear()
+        end_input.send_keys(path[-1].replace("-", " ").replace("_", " ").title())
+        time.sleep(2)
+        end_input.send_keys(Keys.RETURN)
+        start_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div/form/div[5]/button[2]')
+        start_button.click()
     else:
-        return None, "Invalid Website"
+        return None, "Invalid Website or Login Info"
 
     start_time_millis = int(time.time() * 1000)
     time_waiting = 0
     prev_page = path[0]
+    page_load = True
     for page in path[1:]:
-        print("Starting click for " + page)
         next_page_url_encoded = f"{search_url}{quote(page)}"
         next_page_url_unencoded = f"{search_url}{page}"
         page_link = None
         while page_link == None:
+            if website == "wikispeedruns.com" and page_load:
+                time.sleep(1)
+                page_load = False
             try:
                 page_link = driver.find_element(By.CSS_SELECTOR, f'a[href="{next_page_url_unencoded}"]')
             except:
@@ -135,14 +183,14 @@ def run_wiki_bot(path, website="wikipedia.org"):
                     if prev_page.replace("-", " ").replace("_", " ").lower == page.replace("-", " ").replace("_", " ").lower():
                         print("Skipping page, duplicate redirect found")
                         continue
-                    print(f"Couldn't find {next_page_url_unencoded}... Waiting")
                     page_link = None
-                    time.sleep(0.25)
-                    time_waiting += 0.25
+                    time.sleep(sleep_time)
+                    time_waiting += sleep_time
 
         if page_link != None: 
             prev_page = page
             driver.execute_script("arguments[0].click();", page_link)
+            page_load = True
     end_time_millis = int(time.time() * 1000)
     total_time = end_time_millis - start_time_millis
     return total_time, time_waiting
@@ -184,13 +232,27 @@ while command != "q" and command != "quit":
         print(path)
         run_bot = input("Do you want to run the automatic bot for this path? (Y/N): ")
         if run_bot.upper() == "Y" or run_bot.upper() == "YES":
-            website = input("Do you want to run this bot on wikispeedruns.com or wikipedia.com. Type 1 for wikispeedruns.com anything else will default to wikipedia.com: ")
-            if website == "1":
-                total_time, wait_time = run_wiki_bot(path, "wikispeedruns.com")
+            print("Do you want to run this bot on wikispeedruns.com, wikispeedrun.org or wikipedia.com?")
+            website = input("If you don't want to type out the whole link you can type 1 for wikispeedruns.com, 2 for wikispeedrun.org anything else will default to wikipedia.com: ")
+            if website == "1" or website == "wikispeedruns.com":
+                in_lobby = input("Are you in a lobby for wikispeedruns.com? (Y/N): ")
+                if in_lobby.upper() == "N" or in_lobby.upper() == "NO":
+                    total_time, wait_time = run_wiki_bot(path, "wikispeedruns.com")
+                else:
+                    login_info = {}
+                    login_info['lobby_number'] = input("Input lobby number here (the numbers after the last / in the url): ")
+                    login_info['lobby_password'] = input("Input lobby password here: ")
+                    login_info['username'] = input("Put the name you will go as here: ")
+                    login_info['prompt_number'] = input("In the lobby there should be a prompt # column for different challenges. Input the prompt #: ")
+                    total_time, wait_time = run_wiki_bot(path, "wikispeedruns.com", login_info)
+            elif website == "2" or website == "wikispeedrun.org":
+                total_time, wait_time = run_wiki_bot(path, "wikispeedrun.org")
             else:
                 total_time, wait_time = run_wiki_bot(path)
 
-        print(f"Total time to completion: {total_time / 1000} seconds")
-        print(f"Total wait/load time: {wait_time} seconds")
+        if total_time != None:
+            print(f"Total time to completion: {total_time / 1000} seconds")
+            print(f"Total wait/load time: {wait_time} seconds")
+            print(f"Time excluding wait/load time: {(total_time / 1000) - wait_time} seconds")
 
     command = input("Type quit to exit the program or anything else to find a path between two wikipedia pages: ")
